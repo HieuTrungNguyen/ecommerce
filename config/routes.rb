d982cd8432db003bda136285eb6fed445d100e41
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2019-03-04 21:46:32 +0700

    init model & Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/show", to: "static_pages#show"
+  get "/cart", to: "static_pages#cart"
 end
