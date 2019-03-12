c9e86332d5d28b15a5216bbc3e1a97480e1ec29a
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2019-03-12 23:11:48 +0700

    Update hard cart

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,10 @@ module CartsHelper
   def set_cart
     @cart = session[:cart] ||= {}
   end
+
+  def total_price_of_cart
+    @product_of_current_cart.map do |product|
+      product.price * product.quantity_in_cart
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :carts, only: [:index]
   get "add_cart/:id_product", to: "carts#add_cart", as: :add_cart
   get "remove_cart/:id_product", to: "carts#remove_cart", as: :remove_cart
+  put "update_hard_cart", to: "carts#update_hard_cart", as: :update_hard_cart
 
   namespace :admin do
     resources :categories
