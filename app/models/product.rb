2db71e0a99a8f870e2c4a409c478785012de7188
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2019-03-12 20:47:33 +0700

    Create cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: Settings.product.name_length}
   validates :price, :quantity, :image, presence: true
+
+  scope :load_product_by_ids, -> product_ids {where id: product_ids}
 end
