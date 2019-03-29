61b8b2b75abef78929b5c84e6e391275a3398548
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2019-03-29 16:50:18 +0700

    Manage Category in admin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :load_categories, only: [:home]
+
   def home
     @products = Product.load_product.order_by_id.paginate page: params[:page],
       per_page: Settings.per_page
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
 
+  accepts_nested_attributes_for :products, allow_destroy: true
+
   validates :name, presence: true, length: {maximum: Settings.category.name_length}
 end
