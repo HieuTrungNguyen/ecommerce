11b6511dd0933ec7a50cbd7fde4222510cba4486
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2019-04-01 22:34:56 +0700

    Fix view

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,14 @@
 module StaticPagesHelper
+  def status_bg status
+    case status
+    when Settings.statuses.cancelled
+      Settings.warning
+    when Settings.statuses.rejected
+      Settings.danger
+    when Settings.statuses.approved
+      Settings.success
+    else
+      Settings.light
+    end
+  end
 end
